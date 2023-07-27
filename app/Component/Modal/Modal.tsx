ee6e6ca26dbe16dd4552e72f93752813a178463e
Yash-Gupta13 <yashuu1308@gmail.com>
2023-07-27 17:40:47 +0530

    prism error fixed

--- app/Component/Modal/Modal.tsx
+++ app/Component/Modal/Modal.tsx
@@ -1,10 +1,11 @@
-'use client'
-import React, { useCallback, useEffect, useState } from 'react'
-import { IoMdClose } from 'react-icons/io';
-import Button from '../Button';
+'use client';
 
+import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
-interface Modalprops {
+import Button from "../Button";
+
+interface ModalProps {
     isOpen?: boolean;
     onClose: () => void;
     onSubmit: () => void;
@@ -15,13 +16,22 @@ interface Modalprops {
     disabled?: boolean;
     secondaryAction?: () => void;
     secondaryActionLabel?: string;
-
 }
 
-const Modal: React.FC<Modalprops> = ({
-    isOpen, onClose, onSubmit, title, body, footer, actionLabel, disabled, secondaryAction, secondaryActionLabel
+const Modal: React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    actionLabel,
+    footer,
+    disabled,
+    secondaryAction,
+    secondaryActionLabel
 }) => {
     const [showModal, setShowModal] = useState(isOpen);
+
     useEffect(() => {
         setShowModal(isOpen);
     }, [isOpen]);
@@ -35,7 +45,7 @@ const Modal: React.FC<Modalprops> = ({
         setTimeout(() => {
             onClose();
         }, 300)
-    }, [disabled, onclose]);
+    }, [onClose, disabled]);
 
     const handleSubmit = useCallback(() => {
         if (disabled) {
@@ -43,8 +53,7 @@ const Modal: React.FC<Modalprops> = ({
         }
 
         onSubmit();
-
-    }, [disabled, onSubmit]);
+    }, [onSubmit, disabled]);
 
     const handleSecondaryAction = useCallback(() => {
         if (disabled || !secondaryAction) {
@@ -52,103 +61,116 @@ const Modal: React.FC<Modalprops> = ({
         }
 
         secondaryAction();
-
-    }, [disabled, secondaryAction]);
+    }, [secondaryAction, disabled]);
 
     if (!isOpen) {
         return null;
     }
+
     return (
         <>
-            <div className='justify-center 
-        item-center
-        flex
-        overflow-x-hidden
-        overflow-y-auto
-        fixed
-        inset-0
-        z-50
-        outline-none
-        focus:outline-none
-        bg-neutral-800/70'>
-                <div className='relative w-full 
-                md:w-4/6
-                lg:w-3/6
-                xl:w-2/5
-                my-6
-                mx-auto
-                h-full
-                lg:h-auto
-                md:h-auto'>
-                    <div className={`translate
-                    duration-300
-                    h-full 
-                    ${showModal ? 'translate-y-0' : 'translate-y-full'}
-                    ${showModal ? 'opacity-100' : 'opacity-0'}`}>
-                        <div className='translate
-                        h-full
-                        lg:h-auto
-                        md:h-auto
-                        border-0
-                        rounded-lg
-                        shadow-lg
-                        relative
-                        flex
-                        flex-col
-                        w-full
-                        bg-white
-                        outline-none
-                        focus:ouline-none'
+            <div
+                className="
+          justify-center 
+          items-center 
+          flex 
+          overflow-x-hidden 
+          overflow-y-auto 
+          fixed 
+          inset-0 
+          z-50 
+          outline-none 
+          focus:outline-none
+          bg-neutral-800/70
+        "
+            >
+                <div className="
+          relative 
+          w-full
+          md:w-4/6
+          lg:w-3/6
+          xl:w-2/5
+          my-6
+          mx-auto 
+          h-full 
+          lg:h-auto
+          md:h-auto
+          "
+                >
+                    {/*content*/}
+                    <div className={`
+            translate
+            duration-300
+            h-full
+            ${showModal ? 'translate-y-0' : 'translate-y-full'}
+            ${showModal ? 'opacity-100' : 'opacity-0'}
+          `}>
+                        <div className="
+              translate
+              h-full
+              lg:h-auto
+              md:h-auto
+              border-0 
+              rounded-lg 
+              shadow-lg 
+              relative 
+              flex 
+              flex-col 
+              w-full 
+              bg-white 
+              outline-none 
+              focus:outline-none
+            "
                         >
-                            <div className='flex
-                            items-center
-                            p-6
-                            rounded-t
-                            justify-center
-                            relative
-                            border-b-[1px]'
+                            {/*header*/}
+                            <div className="
+                flex 
+                items-center 
+                p-6
+                rounded-t
+                justify-center
+                relative
+                border-b-[1px]
+                "
                             >
                                 <button
+                                    className="
+                    p-1
+                    border-0 
+                    hover:opacity-70
+                    transition
+                    absolute
+                    left-9
+                  "
                                     onClick={handleClose}
-                                    className='p-1
-                                border-0
-                                hover:opacity-70
-                                transition
-                                absolute
-                                right-9'
                                 >
                                     <IoMdClose size={18} />
                                 </button>
-                                <div className='text-lg font-semibold'>{title}</div>
-
+                                <div className="text-lg font-semibold">
+                                    {title}
+                                </div>
                             </div>
-                            <div
-                                className='relative 
-                                p-6 
-                                flex-auto'
-                            >
+                            {/*body*/}
+                            <div className="relative p-6 flex-auto">
                                 {body}
-
                             </div>
-                            <div
-                                className='flex flex-col
-                                p-6 gap-2'
-                            >
+                            {/*footer*/}
+                            <div className="flex flex-col gap-2 p-6">
                                 <div
-                                    className='
-                                    flex
-                                    flex-row
-                                    items-center
-                                    gap-4
-                                    w-full'
+                                    className="
+                    flex 
+                    flex-row 
+                    items-center 
+                    gap-4 
+                    w-full
+                  "
                                 >
                                     {secondaryAction && secondaryActionLabel && (
-
                                         <Button
-                                            outline
                                             disabled={disabled}
                                             label={secondaryActionLabel}
                                             onClick={handleSecondaryAction}
+                                            outline
                                         />
                                     )}
                                     <Button
@@ -156,24 +178,15 @@ const Modal: React.FC<Modalprops> = ({
                                         label={actionLabel}
                                         onClick={handleSubmit}
                                     />
-
                                 </div>
                                 {footer}
-
                             </div>
-
-
-
                         </div>
-
                     </div>
-
                 </div>
-
-
             </div>
         </>
-    )
+    );
 }
 
-export default Modal
+export default Modal;
